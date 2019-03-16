7379c84747e44ce84b9b3e8356df4eb07ba4eb29
Ernie Val <erndval@gmail.com>
2019-03-16 03:57:22 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,8 @@
 def oxford_comma(array)
 if array.size == 2
   array.join(" and ")
+  elsif array.size == 1
+  array.join
 else
   last_element = array.pop.to_s
   last_element_with_and = "and #{last_element}"
