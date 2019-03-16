70833104ac4995e856295861115bdd9d2809e7c9
Ernie Val <erndval@gmail.com>
2019-03-16 03:52:42 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,6 +2,9 @@ def oxford_comma(array)
 if array.size == 2
   array.join(" and ")
 else
-  array.insert(-2, "and").join(", ")
+  last_element = array.pop
+  
+  oxford array = array.join(", ")
+  
 end
 end
